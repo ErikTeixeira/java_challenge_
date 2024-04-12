608b178b42972fa19f9c6bb061e6703767321bd5
ErikTeixeira <erik.barbosa515@gmail.com>
2024-04-12 18:29:20 -0300

    entitys criadas

---
+++ src/main/java/br/com/fiap/java_challenge/entity/PreferenciaViagem.java
@@ -0,0 +1,48 @@
+package br.com.fiap.java_challenge.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+
+@Entity
+@Table(name = "TB_PREFERENCIA_VIAGEM")
+public class PreferenciaViagem {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_PREFERENCIAVIAGEM")
+    @SequenceGenerator(name = "SQ_PREFERENCIAVIAGEM", sequenceName = "SQ_PREFERENCIAVIAGEM", allocationSize = 1)
+    @Column(name = "ID_PREFERENCIAVIAGEM")
+    private Long id;
+
+    private String tipo_culinaria;
+
+    private String restricoes_alimentares;
+
+    private String tipo_transporte;
+
+    private String tipo_hospedagem;
+
+    private String viaja_sozinho;
+
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(
+            name = "USUARIO",
+            referencedColumnName = "ID_USUARIO",
+            foreignKey = @ForeignKey(
+                    name = "FK_PREFERENCIA_VIAGEM_USUARIO"
+            )
+    )
+    private Usuario usuario;
+
+}
